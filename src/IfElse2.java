bde6630c8a4ee60c0caa892d1c9e8c5050440fa4
LÍVIA MARTINS GUIMARAES <livia.guimaraes3@senaisp.edu.br>
2026-07-30 14:24:41 -0300

    feat: festa da grazi

---
+++ src/IfElse2.java
@@ -0,0 +1,15 @@
+public class IfElse2 {
+//FESTA DA GRAZI
+    static void main() {
+
+        int idade = Integer.parseInt(IO.readln("Qual sua idade: "));
+
+        if (idade < 18){
+            IO.println("Pode entrar e curtir");
+        }else{
+            IO.println("Ta velho dorme:)");
+        }
+
+
+    }
+}
