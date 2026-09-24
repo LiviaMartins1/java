215c089080a6f654aef965c505967e1ab090f38e
LÍVIA MARTINS GUIMARAES <livia.guimaraes3@senaisp.edu.br>
2026-09-24 13:49:45 -0300

    feat: Trabalhando com ifelse e while

---
+++ src/Segredotx.java
@@ -0,0 +1,46 @@
+public class Segredotx {
+    static void main() {
+        String segredo ="Be" ;
+
+        String pergunta ="";
+
+            while(!segredo.equals(pergunta)){
+                IO.println("Você errou");
+                pergunta = IO.readln("Digite o numero secreto: ");
+            }
+        IO.println("Parabens você acertou!!!");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
